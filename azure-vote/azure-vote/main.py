93521c8dc9908e613167c3388c1cfcaed93d1589
zoemarin <zoe@marin.info>
2023-02-23 16:00:57 +0100

    change

--- azure-vote/azure-vote/main.py
+++ azure-vote/azure-vote/main.py
@@ -10,20 +10,15 @@ app = Flask(__name__)
 # Load configurations from environment or config file
 app.config.from_pyfile('config_file.cfg')
 
-if ("VOTE1VALUE" in os.environ and os.environ['VOTE1VALUE']):
-    button1 = os.environ['VOTE1VALUE']
-else:
-    button1 = app.config['VOTE1VALUE']
-
-if ("VOTE2VALUE" in os.environ and os.environ['VOTE2VALUE']):
-    button2 = os.environ['VOTE2VALUE']
-else:
-    button2 = app.config['VOTE2VALUE']
-
 if ("TITLE" in os.environ and os.environ['TITLE']):
     title = os.environ['TITLE']
 else:
     title = app.config['TITLE']
+
+if ("TPARAGRAPH1" in os.environ and os.environ['PARAGRAPH1']):
+    paragraph = os.environ['PARAGRAPH1']
+else:
+    paragraph = app.config['PARAGRAPH1']
 
 # Redis configurations
 redis_server = os.environ['REDIS']
@@ -45,44 +40,12 @@ if app.config['SHOWHOST'] == "true":
     title = socket.gethostname()
 
 # Init Redis
-if not r.get(button1): r.set(button1,0)
-if not r.get(button2): r.set(button2,0)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-
-    if request.method == 'GET':
-
-        # Get current values
-        vote1 = r.get(button1).decode('utf-8')
-        vote2 = r.get(button2).decode('utf-8')            
-
-        # Return index with values
-        return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
-
-    elif request.method == 'POST':
-
-        if request.form['vote'] == 'reset':
-            
-            # Empty table and return results
-            r.set(button1,0)
-            r.set(button2,0)
-            vote1 = r.get(button1).decode('utf-8')
-            vote2 = r.get(button2).decode('utf-8')
-            return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
-        
-        else:
-
-            # Insert vote result into DB
-            vote = request.form['vote']
-            r.incr(vote,1)
-            
-            # Get current values
-            vote1 = r.get(button1).decode('utf-8')
-            vote2 = r.get(button2).decode('utf-8')  
-                
-            # Return results
-            return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
+    # Get current values
+    # Return index with values
+    return render_template("index.html",paragraph=paragraph, title=title)
 
 if __name__ == "__main__":
     app.run()
